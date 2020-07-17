e7ccb407f51fee619401d3e41cf78860ce8e902c
MariamElmorshedy <60477600+MariamElmorshedy@users.noreply.github.com>
2020-07-17 21:24:23 +0200

    Json

---
+++ Project3/Petition.swift
@@ -0,0 +1,16 @@
+//
+//  Petition.swift
+//  Project3
+//
+//  Created by Mariam on 7/12/20.
+//  Copyright © 2020 Mariam. All rights reserved.
+//
+
+import Foundation
+
+struct Petition: Codable
+{
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project3/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project3
+//
+//  Created by Mariam on 7/12/20.
+//  Copyright © 2020 Mariam. All rights reserved.
+//
+
+import Foundation
+struct Petitions: Codable {
+    var results: [Petition]
+}
